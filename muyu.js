11f622f5f27a870aa2149c294164eb6783ba3397
Liliana <liliana69012@gmail.com>
2024-05-14 15:59:40 +0800

    Update muyu.js

--- muyu.js
+++ muyu.js
@@ -1,6 +1,6 @@
 var counter = 0;
 var counterElement = document.getElementById('counter');
-var textElement = document.getElementById('text');
+var quoteElement = document.getElementById('quote');
 
 function beat() {
     counter++;
@@ -11,16 +11,15 @@ function beat() {
 function reset() {
     counter = 0;
     counterElement.innerText = '累计积累功德' + counter + '次';
+    fetchQuote();
 }
 
 function fetchQuote() {
-    textElement.style.opacity = '0';
     fetch('https://quotes.rest/qod?language=en')
         .then(response => response.json())
         .then(data => {
             var quote = data.contents.quotes[0].quote;
-            textElement.innerText = quote;
-            textElement.style.opacity = '1';
+            quoteElement.innerText = quote;
         })
         .catch(error => console.error('Error:', error));
 }
